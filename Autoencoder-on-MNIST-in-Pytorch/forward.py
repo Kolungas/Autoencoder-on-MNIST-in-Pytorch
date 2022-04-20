d94eada1d6061c699c592710923c69127745ec1c
Exodus <illia.Khlopuk@lnu.edu.ua>
2022-04-20 17:07:47 +0300

    Added forward running for test data

---
+++ Autoencoder-on-MNIST-in-Pytorch/forward.py
@@ -0,0 +1,21 @@
+from Autoencoder import Autoencoder, testset
+import matplotlib.pyplot as plt
+import torch
+
+model = Autoencoder().cpu()
+model.load_state_dict(torch.load('Models/model_e14.pt'))
+model.eval()
+
+n = [247]
+
+for data in testset:
+    img, num = data
+    plt.imshow(img.reshape([28, 28]))
+    print(num)
+    plt.show()
+
+    img = img.reshape([1, 1, 28, 28])
+    img = torch.autograd.Variable(img).cpu()
+    output = model(img)
+    plt.imshow(output.detach().numpy().reshape([28, 28]))
+    plt.show()
